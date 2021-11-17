b1a52133d5138759c65eba293cda573c6e996113
abegovac2 <begovac5@gmail.com>
2021-11-17 17:34:18 +0100

    Refactoring

--- controller/testovi.js
+++ controller/testovi.js
@@ -1,48 +1,25 @@
-class HelperClass{
-    static dajTestove(json) {
+var TestoviParser = (function () {
 
-        let test = this.dajBrojcaniAtribut("tests", json);
-        if (test == null) return null;
-        let pass = this.dajBrojcaniAtribut("passes", json);
-        if (pass == null) return null;
-        return (test == 0) ? 0 : pass / test;
-    }
-    
-    static dajPaleTestove(json) {
-        let res = (/\"(failures)\"\s?:\s?\[\{?.*?\}?\]/g).exec(json);
-        if(res == null) return null;
-        res = res[0].match(/\"fullTitle\"\s?:\s?\".*?\"/g);
-        return res == null ? 0 : res.map((str) => str.split(/\"fullTitle\"\s?:\s?/g)[1]);
-    }
-    
-    static kreirajObjekat(json) {
-        let [tacnost, ...greske] = json.replace(/[\{\}]/g, '').split(/\s?\,\s?/i);
-        tacnost = tacnost.replace(/[\"\s]/g, '').split(/:/g);
-        let reg = /(\s?:\s?|\"\,\")/g;
-        greske = greske.join(',')
-            .replace(/[\[\]]/g, '')
-            .split(reg)
-            .filter((str) => !reg.test(str))
-            .map((str) => str.replace(/\"/g, ''));
-        if(greske[1].length == 0) greske.pop();
-        let obj = {};
-        obj[tacnost[0]] = tacnost[1];
-        obj[greske.shift()] = greske.map((str) => `"${str}"`);
-        return obj;
+    const dajJSONObj = function (json) {
+        let jsonObj;
+        try {
+            jsonObj = JSON.parse(json);
+            if (
+                jsonObj == null ||
+                jsonObj.stats == null ||
+                jsonObj.stats.passes == null ||
+                jsonObj.stats.tests == null ||
+                jsonObj.failures == null ||
+                jsonObj.tests == null
+            )
+                throw "Greska";
+        } catch (err) {
+            jsonObj = null
+        }
+        return jsonObj;
     }
 
-    static dajBrojcaniAtribut(atribut, json) {
-        let reg = new RegExp("\\\"(" + atribut + ")\\\"\\\s?:\\\s?(\\\d+)", "g");
-        let test = reg.exec(json);
-        if (test == null) return null;
-    
-        test = (/\d+/g).exec(test);
-        test = parseInt(test[0]);
-    
-        return test;
-    }
-    
-    static areEqual(arr1, arr2) {
+    const areEqual = function (arr1, arr2) {
         let n = arr1.length;
         let m = arr2.length;
         if (n != m) return false;
@@ -53,58 +30,63 @@ class HelperClass{
                 return false;
         return true;
     }
-}
 
-
-class TestoviParser {
-    dajTacnost(json) {
-        let tacnost = HelperClass.dajTestove(json);
-        if (tacnost == null) return `{"tacnost" : "0%", "greske" : "Testovi se ne mogu izvrsiti"}`;
-        let res = `{"tacnost" : "${tacnost * 100}%", "greske" : [`;
-        if (tacnost == 1) res += "]}";
-        else {
-            let text = HelperClass.dajPaleTestove(json);
-            if (text == null) return `{"tacnost" : "0%", "greske" : "Testovi se ne mogu izvrsiti"}`;
-            res += (text == 0 ? '' : text) + "]}";
+    const dajTacnost = function (json) {
+        let jsonObj;
+        try {
+            jsonObj = JSON.parse(json);
+            if (
+                jsonObj == null ||
+                jsonObj.stats == null ||
+                jsonObj.stats.passes == null ||
+                jsonObj.stats.tests == null ||
+                jsonObj.failures == null)
+                throw "Greska";
+        } catch (err) {
+            return "{\"tacnost\":\"0%\",\"greske\":\"Testovi se ne mogu izvrsiti\"}"
         }
-        return res;
-    }
+        let res = {};
 
-    porediRezultate(rezultat1, rezultat2) {
-        let obj1 = HelperClass.kreirajObjekat(this.dajTacnost(rezultat1));
-        let obj2 = HelperClass.kreirajObjekat(this.dajTacnost(rezultat2));
-        let res = `{"promjena":"`;
-        if (HelperClass.areEqual(obj1.greske, obj2.greske)) {
-            obj2.greske = obj2.greske.map((str) => "\"" + str + "\"");
-            res += obj2.tacnost + `","greske":[${obj1.greske.sort()}]}`;
+        res['tacnost'] = (jsonObj.stats.passes / jsonObj.stats.tests * 100) + "%";
+        res['greske'] = jsonObj.failures.map(x => x.fullTitle);
+        return JSON.stringify(res).replace(/[\n\t]/g, '');
+    };
+
+    const porediRezultate = function (rezultat1, rezultat2) {
+        let obj1 = dajJSONObj(rezultat1);
+        if (obj1 == null) return "Greska";
+
+        let obj2 = dajJSONObj(rezultat2);
+        if (obj2 == null) return "Greska";
+
+        let res = {};
+
+        obj1.failures = obj1.failures.map(x => x.fullTitle);
+        obj2.failures = obj2.failures.map(x => x.fullTitle);
+
+        if (areEqual(obj1.failures, obj2.failures)) {
+            res['promjena'] = (obj2.stats.passes / obj2.stats.tests * 100) + "%";
+            res['greske'] = obj1.failures == null ? [] : obj1.failures.sort();
         } else {
-            let promjena = [];
-            for (let i = 0; i < obj1.greske.length; i++)
-                if (!obj2.greske.includes(obj1.greske[i])) promjena.push(obj1.greske[i]);
-            promjena.sort();
-            let promjena1 = promjena.length;
-            let brTestova2 = HelperClass.dajBrojcaniAtribut("tests", rezultat2);
-            promjena1 = (promjena1 + obj2.greske.length) / (promjena1 + brTestova2) * 100;
-            if(obj2.greske.length > 0) promjena.push(obj2.greske.sort());
-            promjena1 = promjena1.toFixed(2);
-            res += promjena1 + `%","greske":[${promjena}]}`;
-        }
-        return res;
-    }
+            let greske = [];
+            for (let i = 0; i < obj1.failures.length; i++)
+                if (!obj2.failures.includes(obj1.failures[i]))
+                    greske.push(obj1.failures[i]);
 
-}
+            greske.sort();
+            let promjena = (greske.length + obj2.stats.failures) / (greske.length + obj2.stats.tests) * 100;
+            promjena = promjena.toFixed(2);
+            if (obj2.failures != null) greske = greske.concat(obj2.failures.sort());
+            res['promjena'] = promjena + "%";
+            res['greske'] = greske;
+        }
+        return JSON.stringify(res).replace(/[\n\t]/g, '');;
+    };
+
+    return {
+        dajTacnost: dajTacnost,
+        porediRezultate: porediRezultate
+    };
+}());
 
 module.exports = TestoviParser;
-//let json = "{\"stats\":{\"suites\":2,\"tests\":2,\"passes\":1,\"pending\":0,\"failures\":1,\"start\":\"2021-11-05T15:00:26.343Z\",\"end\":\"2021-11-05T15:00:26.352Z\",\"duration\":9},\"tests\":[{\"title\":\"should draw 3 rows when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 3 rows when parameter are 2,3\",\"file\":null,\"duration\":1,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 2 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}}],\"pending\":[],\"failures\":[{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 2 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}}],\"passes\":[{\"title\":\"should draw 3 rows when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 3 rows when parameter are 2,3\",\"file\":null,\"duration\":1,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}}]}";
-//let json = "{\"tacnost\" : \"50%\", \"greske\" : [\"Tabela crtaj() should draw 2 columns in row 2 when parameter are 2,3\",\"Tabela crtaj() should draw 3 columns in row 2 when parameter are 2,3\",\"Tabela crtaj() should draw 4 columns in row 2 when parameter are 2,3\"}";
-//let json1 = "{\"stats\":{\"suites\":2,\"tests\":5,\"passes\":1,\"pending\":0,\"failures\":4,\"start\":\"2021-11-05T15:00:26.343Z\",\"end\":\"2021-11-05T15:00:26.352Z\",\"duration\":9},\"tests\":[{\"title\":\"should draw 3 rows when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 3 rows when parameter are 2,3\",\"file\":null,\"duration\":1,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 2 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}}],\"pending\":[],\"failures\":[{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 2 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}}, {\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 3 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 4 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 5 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}}],\"passes\":[{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 6 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}}]}";
-//let json2 = "{\"stats\":{\"suites\":2,\"tests\":5,\"passes\":1,\"pending\":0,\"failures\":4,\"start\":\"2021-11-05T15:00:26.343Z\",\"end\":\"2021-11-05T15:00:26.352Z\",\"duration\":9},\"tests\":[{\"title\":\"should draw 3 rows when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 3 rows when parameter are 2,3\",\"file\":null,\"duration\":1,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 2 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}}],\"pending\":[],\"passes\":[{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 2 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}}, {\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 3 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 4 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 5 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}}],\"failures\":[{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 6 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}}]}";
-      
-//let json1 = "{\"stats\":{\"suites\":2,\"tests\":5,\"passes\":0,\"pending\":0,\"failures\":5,\"start\":\"2021-11-05T15:00:26.343Z\",\"end\":\"2021-11-05T15:00:26.352Z\",\"duration\":9},\"tests\":[{\"title\":\"should draw 3 rows when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 3 rows when parameter are 2,3\",\"file\":null,\"duration\":1,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 2 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}}],\"pending\":[],\"failures\":[{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 2 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 3 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 4 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 5 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 6 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}}],\"passes\":[]}";
-//let json2 = "{\"stats\":{\"suites\":2,\"tests\":5,\"passes\":5,\"pending\":0,\"failures\":0,\"start\":\"2021-11-05T15:00:26.343Z\",\"end\":\"2021-11-05T15:00:26.352Z\",\"duration\":9},\"tests\":[{\"title\":\"should draw 3 rows when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 3 rows when parameter are 2,3\",\"file\":null,\"duration\":1,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 2 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}}],\"pending\":[],\"failures\":[],\"passes\":[{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 2 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 3 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 4 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 5 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 6 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}}]}";
-//let json1 = "{\"stats\":{\"suites\":2,\"tests\":0,\"passes\":0,\"pending\":0,\"failures\":0,\"start\":\"2021-11-05T15:00:26.343Z\",\"end\":\"2021-11-05T15:00:26.352Z\",\"duration\":9},\"tests\":[{\"title\":\"should draw 3 rows when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 3 rows when parameter are 2,3\",\"file\":null,\"duration\":1,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 2 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}}],\"pending\":[],\"failures\":[],\"passes\":[]}";
-//let json2 = "{\"stats\":{\"suites\":2,\"tests\":5,\"passes\":0,\"pending\":0,\"failures\":5,\"start\":\"2021-11-05T15:00:26.343Z\",\"end\":\"2021-11-05T15:00:26.352Z\",\"duration\":9},\"tests\":[{\"title\":\"should draw 3 rows when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 3 rows when parameter are 2,3\",\"file\":null,\"duration\":1,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 2 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}}],\"pending\":[],\"failures\":[{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 2 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 3 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 4 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 5 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}},{\"title\":\"should draw 2 columns in row 2 when parameter are 2,3\",\"fullTitle\":\"Tabela crtaj() should draw 6 columns in row 2 when parameter are 2,3\",\"file\":null,\"duration\":0,\"currentRetry\":0,\"speed\":\"fast\",\"err\":{}}],\"passes\":[]}";
-                                     
-//var testoviParser = new TestoviParser();
-//console.log(testoviParser.porediRezultate(json2, json1));
-//console.log(testoviParser.dajBrojcaniAtribut('tests', json));
