79a5a61679a4993e29ecef195788ea67ff20d8b7
abegovac2 <begovac5@gmail.com>
2022-01-03 20:54:34 +0100

    prvi zadatak spirala 3

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "225883",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "html", "unosVjezbi.html"));
+});
+
+let getVjezbeData = function (req, res) {
+  let obj = {};
+  fs.readFile("./vjezbe.csv", "utf-8", function (err, data) {
+    if (err) throw err;
+    data = data.split("\n");
+    data.shift();
+    data = data.map((el) => parseInt(el));
+    obj["brojVjezbi"] = data.length;
+    obj["brojZadataka"] = data;
+    res.send(obj);
+  });
+};
+
+app
+  .route("/vjezbe")
+  .post(function (req, res, next) {
+    let { brojVjezbi, brojZadataka } = req.body;
+    //brojVjezbi = parseInt(brojVjezbi);
+    //brojZadataka = brojZadataka.split(",").map((x) =>  parseInt(x));
+    let sendErr =
+      brojVjezbi == undefined ||
+      brojVjezbi < 1 ||
+      brojVjezbi > 15 ||
+      brojVjezbi != brojZadataka.length;
+    let errMess = "Pogresan parametar ";
+    if (sendErr) errMess += "brojVjezbi";
+    if (brojZadataka == undefined) {
+      errMess += `${sendErr ? "," : ""}brojZadataka`;
+      sendErr = true;
+    } else
+      for (let i = 0; i < brojZadataka.length; ++i) {
+        const br = brojZadataka[i];
+        if (br < 0 || br > 10) {
+          errMess += `${sendErr ? "," : ""}z${i}`;
+          sendErr = true;
+        }
+      }
+    if (sendErr) {
+      res.send({ status: "error", data: errMess });
+    } else {
+      let data = "brojZadatakaPoVježbi\n" + brojZadataka.join("\n");
+      fs.writeFile("vjezbe.csv", data, function (err) {
+        if (err) {
+          res.send({
+            status: "error",
+            data: "Greška pri zapisu vježbi u csv.",
+          });
+        } else {
+          next();
+        }
+      });
+    }
+  }, getVjezbeData)
+  .get(getVjezbeData);
+
+app.listen(3000);
--- public/js/VjezbeAjax.js
+++ public/js/VjezbeAjax.js
@@ -1,18 +1,65 @@
-function dodajInputPolja(divElement, brojVjezbi) {
-  let input = `<input type="number" id="z${1}" name="z${1}" />`;
-  let label = `<label for="z${1}">${"nakav content"}</label>`;
-  brojVjezbi.innerHtml = "";
-  for (let i = 0; i < brojVjezbi; ++i)
-    brojVjezbi.innerHtml +=
+/* 1. zadatak */
+
+function dodajInputPolja(DOMelementDIVauFormi, brojVjezbi) {
+  DOMelementDIVauFormi.innerHTML = "";
+  let num_of_elements = brojVjezbi > 15 ? 15 : brojVjezbi < 0 ? 0 : brojVjezbi;
+  for (let i = 0; i < num_of_elements; ++i) {
+    DOMelementDIVauFormi.innerHTML +=
       `<label for="z${i}">${`z${i}`}</label>\n` +
-      `<input type="number" id="z${i}" name="z${i}" />\n`;
+      `<input type="number" id="z${i}" name="z${i}" /><br>\n`;
+  }
 }
 
-function posaljiPodatke(vjezbeObjekat, callbackFja) {
+function posaljiPodatke(vjezbeObjekat, callBackFja) {
   let ajax = new XMLHttpRequest();
 
-  ajax.onreadystatechange = callbackFja;
+  ajax.onreadystatechange = (err, data) => {
+    if (ajax.readyState == 4 && ajax.status == 200)
+      callBackFja(undefined, ajax.responseText);
+  };
   ajax.open("POST", "http://localhost:3000/vjezbe", true);
+  /*ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  let { brojVjezbi, brojZadataka } = vjezbeObjekat;
+  brojVjezbi = encodeURIComponent(brojVjezbi);
+  brojZadataka = encodeURIComponent(brojZadataka);
+  ajax.send(`brojVjezbi=${brojVjezbi}&brojZadataka=${brojZadataka}`);*/
+  ajax.setRequestHeader("Content-type", "application/json");
+  let jsonStr = JSON.stringify(vjezbeObjekat);
+  ajax.send(jsonStr);
+}
+
+/* 2. zadatak */
+
+function dohvatiPodatke(callBackFja) {
+  let ajax = new XMLHttpRequest();
+  ajax.onreadystatechange = (err, data) => {
+    if (ajax.readyState == 4 && ajax.status == 200)
+      callBackFja(undefined, ajax.responseText);
+  };
+  ajax.open("GET", "http://localhost:3000/vjezbe", true);
   ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  ajax.send(vjezbeObjekat);
+  ajax.send();
+}
+
+function iscrtajVjezbe(divDOMelement, objekat) {
+  divDOMelement.innerHTML = "";
+  for (let i = 0; i < objekat.brojVjezbi; ++i) {
+    let input = `<div>
+    <input id="${`v${i}`}" onclick="iscrtajZadatke(document.getElementById('zadaci${i}'), ${
+      objekat.brojZadataka[i]
+    })" groupname="izbori" type="checkbox" class="otvaranje">
+    <label for="${`v${i}`}">
+        ${`Vjezba${i + 1}`}
+    </label>
+    <div class="zadaci" id="zadaci${i}"></div>`;
+    divDOMelement.innerHTML += input;
+  }
+}
+
+function iscrtajZadatke(vjezbaDOMelement, brojZadataka) {
+  let { children } = vjezbaDOMelement;
+  if (children.length == 0) {
+    for (let j = 0; j < brojZadataka; j++)
+      vjezbaDOMelement.innerHTML += `<div>${`Zadatak${j}`}</div>`;
+  }
 }
